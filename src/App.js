8deafaf2bae1836008b1e489f14551330db5b911
Joshua Stuart <Joshuasstuart@gmail.com>
2023-03-20 13:18:02 -0400

    2nd commit added header and header.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './app.css';
+import Header from './Header'
 
 function App() {
   return (
     <div className="app">
       <h1> Let's build a LinkedIn Clone!</h1>
+
+      {/* Header */}
+        <Header /> 
+      {/* App body */}
+        {/* Sidebar */}
+        {/* Feeds */}
+        {/* Widgets */}
     </div>
   );
 }
