8aaf1e0521144bc7d1b51a45ae7b57f9613e512f
Joshua Stuart <Joshuasstuart@gmail.com>
2023-03-21 13:28:13 -0400

    working on nav/header w/ styling

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccount from "@mui/icons-material/SupervisorAccount";
 
 function Header() {
   return (
-    <div className='header'>
-        <h1> This is the header!</h1>
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+          alt=""
+        />
+
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccount} title="My Network" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
