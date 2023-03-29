a8a64d8188cb26c92a318086e35a243002379501
Joshua Stuart <Joshuasstuart@gmail.com>
2023-03-29 15:52:45 -0400

    Added feed section and styled some components

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,11 @@
 import CreateIcon from "@mui/icons-material/Create";
 import React from "react";
 import "./Feed.css";
+import ImageIcon from "@mui/icons-material/Image";
+import InputOptions from "./InputOptions";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
 
 function Feed() {
   return (
@@ -9,14 +14,19 @@ function Feed() {
         <div className="feed__input">
           <CreateIcon />
           <form>
-            <input type="text"/>
+            <input type="text" />
             <button type="submit">Send</button>
-
           </form>
+        </div>
+        <div className="feed__inputOptions">
+            <InputOptions Icon={ImageIcon} title='Photo' color='#70B5F9'/>
+            <InputOptions Icon={SubscriptionsIcon} title='Video' color='#E7A33E'/>
+            <InputOptions Icon={EventNoteIcon} title='Event' color='#C0CBCD'/>
+            <InputOptions Icon={CalendarViewDayIcon} title='Write article' color='#7FC15E'/>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Feed;
