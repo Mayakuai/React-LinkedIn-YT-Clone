48c5eb37cc6c8b94e79e0588ae9596163044fcea
Joshua Stuart <Joshuasstuart@gmail.com>
2023-03-30 15:01:12 -0400

    added message post area

--- src/Feed.js
+++ src/Feed.js
@@ -6,6 +6,7 @@ import InputOptions from "./InputOptions";
 import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
 import EventNoteIcon from "@mui/icons-material/EventNote";
 import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
+import Post from "./Post";
 
 function Feed() {
   return (
@@ -19,12 +20,26 @@ function Feed() {
           </form>
         </div>
         <div className="feed__inputOptions">
-            <InputOptions Icon={ImageIcon} title='Photo' color='#70B5F9'/>
-            <InputOptions Icon={SubscriptionsIcon} title='Video' color='#E7A33E'/>
-            <InputOptions Icon={EventNoteIcon} title='Event' color='#C0CBCD'/>
-            <InputOptions Icon={CalendarViewDayIcon} title='Write article' color='#7FC15E'/>
+          <InputOptions Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOptions
+            Icon={SubscriptionsIcon}
+            title="Video"
+            color="#E7A33E"
+          />
+          <InputOptions Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOptions
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7FC15E"
+          />
         </div>
       </div>
+      {/* Posts */}
+      <Post
+        name="Joshua Stuart"
+        description="this is a test"
+        message="Wow this worked"
+      />
     </div>
   );
 }
